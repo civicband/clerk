7d9c232cab771418fa32a6e0ca208ba1159b1e08
Philip James <phildini@phildini.net>
2025-03-12 18:18:30 -0700

    Import what needs importing

--- src/clerk/__init__.py
+++ src/clerk/__init__.py
@@ -1,4 +1,10 @@
 from .cli import cli
+from hookspecs import hookimpl, hookspec, ClerkSpec
+
+import pluggy
+
+pm = pluggy.PluginManager("civicband.clerk")
+pm.add_hookspecs(ClerkSpec)
 
 
 def main() -> None:
