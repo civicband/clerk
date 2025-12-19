332218116209cf7dea0dbf1aa93ba10ff0a4b7c3
Philip James <phildini@phildini.net>
2025-12-19 00:44:05 -0800

    feat: export Fetcher from clerk package

--- src/clerk/__init__.py
+++ src/clerk/__init__.py
@@ -1,18 +1,8 @@
 import logfire
 
-from .cli import cli as cli
-from .hookspecs import ClerkSpec as ClerkSpec
-from .hookspecs import hookimpl as hookimpl
-from .hookspecs import hookspec as hookspec
-from .plugin_loader import load_plugins_from_directory as load_plugins_from_directory
-from .utils import pm as pm
+from .fetcher import Fetcher
 
-# Initialize Logfire
 logfire.configure()
-
-# Instrument SQLite
 logfire.instrument_sqlite3()
 
-
-def main() -> None:
-    cli()
+__all__ = ["Fetcher"]
