83c995fc51c6c91c6ebac607e2c8cba1d79abf27
Philip James <phildini@phildini.net>
2025-12-19 15:35:47 -0800

    Add main entry point and export cli

--- src/clerk/__init__.py
+++ src/clerk/__init__.py
@@ -1,9 +1,15 @@
 import logfire
 
+from .cli import cli
 from .fetcher import Fetcher
 from .hookspecs import hookimpl
 
 logfire.configure()
 logfire.instrument_sqlite3()
 
-__all__ = ["Fetcher", "hookimpl"]
+
+def main():
+    cli()
+
+
+__all__ = ["Fetcher", "hookimpl", "cli", "main"]
